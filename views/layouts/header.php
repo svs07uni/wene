1fcb2066bca0a750550f90f39f54608d8a2e7d72
Silvia <svs07uni@gmail.com>
2018-11-21 22:03:06 -0300

    cambios siendo el usuario guest

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -20,41 +20,41 @@ $apellido = Yii::$app->user->getApellido();
 
             <ul class="nav navbar-nav">
 
-                
-                <li class="dropdown notifications-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-bell-o"></i>
-                        <span class="label label-warning">10</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">Existen 10 convocatorias activas</li>
-                        <li>
-                            <!-- inner menu: contains the actual data -->
-                            <ul class="menu">
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-users text-aqua"></i> 5 pasantías
-                                    </a>
-                                </li>
-                                
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-users text-red"></i> 4 bolsa de trabajo
-                                    </a>
-                                </li>
+                <?php if(!Yii::$app->user->isGuest){ ?>
+                    <li class="dropdown notifications-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-bell-o"></i>
+                            <span class="label label-warning">10</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="header">Existen 10 convocatorias activas</li>
+                            <li>
+                                <!-- inner menu: contains the actual data -->
+                                <ul class="menu">
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-users text-aqua"></i> 5 pasantías
+                                        </a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-users text-red"></i> 4 bolsa de trabajo
+                                        </a>
+                                    </li>
 
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-users text-green"></i> 1 actividad de extensión
-                                    </a>
-                                </li>
-                                
-                            </ul>
-                        </li>
-                        <li class="footer"><a href="#">Ver todo</a></li>
-                    </ul>
-                </li>
-                
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-users text-green"></i> 1 actividad de extensión
+                                        </a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                            <li class="footer"><a href="#">Ver todo</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
                 <!-- User Account: style can be found in dropdown.less -->
 
                 <li class="dropdown user user-menu">
@@ -86,18 +86,20 @@ $apellido = Yii::$app->user->getApellido();
                             </div>
                         </li>
                         <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Perfil</a>
-                            </div>
-                            <div class="pull-right">
-                                <?= Html::a(
-                                    'Salir',
-                                    ['/site/logout'],
-                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
-                                ) ?>
-                            </div>
-                        </li>
+                        <?php if(!Yii::$app->user->isGuest){ ?>
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                                </div>
+                                <div class="pull-right">
+                                    <?= Html::a(
+                                        'Salir',
+                                        ['/site/logout'],
+                                        ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
+                                    ) ?>
+                                </div>
+                            </li>
+                        <?php } ?>
                     </ul>
                 </li>
 
