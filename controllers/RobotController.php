3fc660702ff4d039904c9472800afc689b91fada
Silvia <svs07uni@gmail.com>
2018-11-26 22:56:14 -0300

    transaccion en consulta servicio e insertar en rendimiento_no_acad

--- controllers/RobotController.php
+++ controllers/RobotController.php
@@ -9,14 +9,7 @@ class RobotController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-    	
-        $output = file_get_contents('http://mocovi.uncoma.edu.ar/designa/1.0/aplicacion.php?ah=st5bf6aa9e1c0f07.38686516&ai=designa||3810');
-
-        $datos = json_decode($output);
-        //INICIAR TRANSACCION
-        Rendimiento_no_acad::deleteAll();
-        Rendimiento_no_acad::reponerBD($datos);
-        //return $this->render('index');
+        Rendimiento_no_acad::reponerBD();
     }
 }
 ?>
