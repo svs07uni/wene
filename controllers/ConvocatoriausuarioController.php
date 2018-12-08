1396e49ebb42cc8742ac3406a462c4228b83c8df
lorenzopemac <lorenzopemac@gmail.com>
2018-12-08 14:18:53 -0300

    registrar un postulante

--- controllers/ConvocatoriausuarioController.php
+++ controllers/ConvocatoriausuarioController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 use Yii;
 use app\models\Convocatoria;
+use app\models\Postulante;
 use app\models\ConvocatoriaSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -19,9 +20,10 @@ class ConvocatoriausuarioController extends \yii\web\Controller
 
     public function actionMisconvocatorias(){
         $searchModel = new ConvocatoriaSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-        //$usuario = Yii::$app->user->identity;
-        //print_r($usuario.id);
+        $usuario = Yii::$app->user->identity;
+        $dataProvider = $searchModel->search_postulante(Yii::$app->request->queryParams,$usuario->id_registro);
+        
+        //print_r($usuario->id_registro);
 
         return $this->render('misConvocatorias', [
             'searchModel' => $searchModel,
@@ -29,7 +31,21 @@ class ConvocatoriausuarioController extends \yii\web\Controller
         ]);
     }
 
-    public function postularse(){
+    public function actionPostularse($id_convocatoria){
+        $usuario = Yii::$app->user->identity;
+        $model = new Postulante();
+        $model->id_convocatoria = $id_convocatoria;
+        $model->id_usuario=$usuario->id_registro;
+        $model->id_estado=1;//poner constante en estado postulante, cuando est
+        $model->orden_merito=0;                
+        
+      	//$model->fecha_alta=date("d/m/Y");
+      	if($model->save()){
+      		return $this->redirect(['misconvocatoria']);
+    	} 
+      	
+            
+      
 
     }
 
