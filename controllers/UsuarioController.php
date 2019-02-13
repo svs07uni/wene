6a384da582590c2d93ae4888191149f98b5e239d
Silvia <svs07uni@gmail.com>
2019-02-12 23:09:24 -0300

    redireccion a home

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -15,6 +15,7 @@ use app\models\Publicacion;
 use app\models\PublicacionSearch;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\helpers\Url;
 
 class UsuarioController extends \yii\web\Controller
 {
@@ -98,9 +99,10 @@ class UsuarioController extends \yii\web\Controller
                         'text' => "Ahora puedes acceder a todas las ofertas laborales.",
                         
                     ],
-                'callback' => new \yii\web\JsExpression(' function() { 
-                    $guardar = true
-                }')
+                    'callback' => new \yii\web\JsExpression(' function() { 
+                        $guardar = true;
+                        window.location = "'.Url::to(['/']).'";
+                    }')
                  ]);
                  echo($guardar);
                 if ($guardar = true){
